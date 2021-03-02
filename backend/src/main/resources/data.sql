169a0230d95fc2f82ccc16469c8783a3ddba1745
alcemirjunior <alcemirmjunior@gmail.com>
2021-03-02 00:17:08 -0300

    Oauth security, login

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -7,10 +7,20 @@ INSERT INTO tb_role (authority) VALUES ('MEMBER');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 
-INSERT INTO tb_genre (name) VALUE ('Action');
-INSERT INTO tb_genre (name) VALUE ('Comedy');
-INSERT INTO tb_genre (name) VALUE ('Terror');
+INSERT INTO tb_genre (name) VALUES ('Action');
+INSERT INTO tb_genre (name) VALUES ('Comedy');
+INSERT INTO tb_genre (name) VALUES ('Terror');
 
-INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUE ('spider-man', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 1', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 2', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 3', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 4', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 5', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 6', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 7', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 8', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 9', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis, genre_id) VALUES ('spider-man 10', 'o homem aranha', 2011, 'teste', 'é um filme de 2011', 2);
 
-INSERT INTO tb_genre_movie_
+INSERT INTO tb_review(text, user_id, movie_id) VALUES ('o filme é mto bom, gostei', 2, 5);
+
